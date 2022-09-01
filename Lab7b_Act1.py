52bce85f2eb5cbce24c4e5453ce3788aaf4fbb78
CalebLewis0917 <95330126+CalebLewis0917@users.noreply.github.com>
2022-09-01 15:08:54 -0500

    Add files via upload

---
+++ Lab7b_Act1.py
@@ -0,0 +1,11 @@
+# By submitting this assignment, I agree to the following:
+#   "Aggies do not lie, cheat, or steal, or tolerate those who do."
+#   "I have not given or received any unauthorized aid on this assignment."
+#
+# Names:        Caleb Lewis
+# Section:      521
+# Assignment:   Lab7b
+# Date:         10 25 2021
+
+name = input("What is your name? ")
+print(name + " " + name + " Bo-B" + name[1:] + "\nBanana-Fana Fo-F" + name[1:] + "\nMe Mi Mo-M" + name[1:] + "\n" + name + "!")
